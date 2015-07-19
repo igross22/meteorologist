8445930db832728c1fb666535cd4b161fc1ea255
igross22 <ian.j.gross@gmail.com>
2015-07-19 14:52:34 -0500

    Changes as of 3pm, 7/19

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -17,20 +17,35 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
-    coordinate_address = "https://api.forecast.io/forecast/d780981f224f9a545797186f6e904bfb/" +@latitude + "," + @longitude
+    coordinate_address = "http://maps.googleapis.com/maps/api/geocode/json?address="+url_safe_street_address+"&sensor=false"
     raw_data = open(coordinate_address).read
     require 'json'
     parsed_data = JSON.parse(raw_data)
+    results = parsed_data["results"]
+    first = results[1]
 
-    @current_temperature = "do you work?"
 
-    @current_summary = "@street_address"
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"].to_s
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"].to_s
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    # why do I need to turn variables above into .to_s? Without it I get a warning about "no implicit coversion intro string"
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+
+    temp_coordinate_address = "https://api.forecast.io/forecast/d780981f224f9a545797186f6e904bfb/" +@latitude + "," + @longitude
+    temp_raw_data = open(temp_coordinate_address).read
+    require 'json'
+    temp_parsed_data = JSON.parse(temp_raw_data)
+
+    @current_temperature = temp_parsed_data["currently"]["temperature"]
+
+    @current_summary = temp_parsed_data["currently"]["summary"]
+
+    @summary_of_next_sixty_minutes = temp_parsed_data["minutely"]["summary"]
+
+    @summary_of_next_several_hours = temp_parsed_data["hourly"]["summary"]
+
+    @summary_of_next_several_days = temp_parsed_data["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
